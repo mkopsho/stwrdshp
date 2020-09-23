e1e6d5b99c8fe6db17b6f20b677a32e434011793
mkopsho <michael.kopsho@gmail.com>
2020-09-23 16:03:05 -0400

    Further stubbing for  feature

--- stwrdshp-frontend/src/containers/LikesContainer.js
+++ stwrdshp-frontend/src/containers/LikesContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ParkCard from '../components/ParkCard'
 
 export default class LikesContainer extends Component {
   state = {
@@ -7,7 +8,23 @@ export default class LikesContainer extends Component {
 
   componentDidMount() {
     console.log("LikesContainer mounted")
+    // fetch('http://localhost:3000/parks')
+    //   .then((parks) => {
+    //     return parks.json()
+    //   })
+    //   .then((parks) => {
+    //     this.setState({
+    //       // liked parks
+    //     })
+    //   })
   }
+
+  // renderLikedParks = () => {
+  //   // TODO: use Redux
+  //   return this.state.likes.map((park, index) => {
+  //     return <ParkCard name={park.name} img={park.image} state={park.state} id={park.id} key={index} />
+  //   })
+  // }
 
   render() {
     return (
