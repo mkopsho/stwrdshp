b9a7b28e84e5a5ca9d55e53fc08dbc4591cf02b2
mkopsho <michael.kopsho@gmail.com>
2020-09-23 13:24:57 -0400

    Stubbing out the ability to  a Park for future Lists

--- stwrdshp-frontend/src/components/ParkCard.js
+++ stwrdshp-frontend/src/components/ParkCard.js
@@ -2,13 +2,28 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const ParkCard = (props) => {
+  let likeDisplay
+  if (localStorage.logged_in) { // TODO: use Redux
+    likeDisplay = (
+      <div>
+        {props.name} | {props.state} | LIKE ME
+      </div>
+    )
+  } else {
+    likeDisplay = (
+      <div>
+        {props.name} | {props.state}
+      </div>
+    )
+  }
+
   console.log("I'm being rendered")
   return (
     <div className="park_card" >
       <NavLink to={`/parks/${props.index}`}>
         <p key={props.index}>{props.name}<img src={props.img} alt={"Image of " + props.name}></img></p>
         <div>
-          {props.name} | {props.state}
+          {likeDisplay}
         </div>
       </NavLink>
     </div>
