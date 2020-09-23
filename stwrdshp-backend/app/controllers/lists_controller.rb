078333ce3ff237910754cdfa6c6638b7e05a484d
mkopsho <michael.kopsho@gmail.com>
2020-09-23 16:44:32 -0400

    Validate the uniqueness of our joins on the Faves Model

--- stwrdshp-backend/app/controllers/lists_controller.rb
+++ stwrdshp-backend/app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   def update
     user = User.find_by(username: params[:username])
-    Fave.create(list_id: user.lists.first.id, park_id: params[:park_id])
-    render json: "List create endpoint!"
+    fave_join = Fave.create(list_id: user.lists.first.id, park_id: params[:park_id])
+    render json: fave_join
   end
 end
--- stwrdshp-backend/app/models/fave.rb
+++ stwrdshp-backend/app/models/fave.rb
@@ -1,4 +1,5 @@
 class Fave < ApplicationRecord
   belongs_to :list
   belongs_to :park
+  validates_uniqueness_of :park_id, :scope => :list_id
 end
